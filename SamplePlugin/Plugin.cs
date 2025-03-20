7338242e745ca781dfcb62ade89f151b130dd3fc
antek1085 <antek1085@o2.pl>
2025-03-20 01:46:48 +0100

    Added method to check Logs of players

--- SamplePlugin/Plugin.cs
+++ SamplePlugin/Plugin.cs
@@ -1,9 +1,12 @@
-﻿using Dalamud.Game.Command;
+﻿using System.Diagnostics;
+using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
+using FFXIVClientStructs.FFXIV.Client.Game.Character;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using Lumina.Excel.Sheets;
 using SamplePlugin.Windows;
 
 namespace SamplePlugin;
@@ -16,12 +19,14 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static IClientState ClientState { get; private set; } = null!;
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
     [PluginService] internal static IPluginLog Log { get; private set; } = null!;
+    
+    private const string CommandHelp = "/clogUI";
+    private const string CommandName = "/checklogs";
 
-    private const string CommandName = "/pmycommand";
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
+    public readonly WindowSystem WindowSystem = new("FFXIV Logs Check");
     private ConfigWindow ConfigWindow { get; init; }
     private MainWindow MainWindow { get; init; }
 
@@ -30,24 +35,30 @@ public sealed class Plugin : IDalamudPlugin
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
         // you might normally want to embed resources and load them from the manifest stream
-        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
 
-        ConfigWindow = new ConfigWindow(this);
-        MainWindow = new MainWindow(this, goatImagePath);
+        /*ConfigWindow = new ConfigWindow(this);*/
+        MainWindow = new MainWindow(this);
 
-        WindowSystem.AddWindow(ConfigWindow);
+        /*WindowSystem.AddWindow(ConfigWindow);*/
         WindowSystem.AddWindow(MainWindow);
 
-        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
-        {
-            HelpMessage = "A useful message to display in /xlhelp"
-        });
+        CommandManager.AddHandler(CommandName,
+                                  new CommandInfo(OnCommand)
+                                  {
+                                      HelpMessage = "Check target FFXIV Logs"
+                                  });
+
+        CommandManager.AddHandler(CommandHelp,
+                                  new CommandInfo(OnCommandToggleUi)
+                                  {
+                                      HelpMessage = "Enable Main UI"
+                                  });
 
         PluginInterface.UiBuilder.Draw += DrawUI;
 
         // This adds a button to the plugin installer entry of this plugin which allows
         // to toggle the display status of the configuration ui
-        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
+        /*PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;*/
 
         // Adds another button that is doing the same but for the main ui of the plugin
         PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
@@ -56,6 +67,7 @@ public sealed class Plugin : IDalamudPlugin
         // Use /xllog to open the log window in-game
         // Example Output: 00:57:54.959 | INF | [SamplePlugin] ===A cool log message from Sample Plugin===
         Log.Information($"===A cool log message from {PluginInterface.Manifest.Name}===");
+
     }
 
     public void Dispose()
@@ -67,15 +79,62 @@ public sealed class Plugin : IDalamudPlugin
 
         CommandManager.RemoveHandler(CommandName);
     }
+   
 
-    private void OnCommand(string command, string args)
+    public void OnCommand(string command, string args)
     {
-        // in response to the slash command, just toggle the display status of our main ui
-        ToggleMainUI();
+       CheckFFLogs();
     }
+    public void OnCommandToggleUi(string command, string args)
+    {
+        MainWindow.Toggle();
+    }
+    
+    public void CheckFFLogs()
+    {
+        unsafe
+        {
+            var targetObject = TargetSystem.Instance()->GetTargetObject();
+            
+            if (targetObject == null|| targetObject->ObjectKind != FFXIVClientStructs.FFXIV.Client.Game.Object.ObjectKind.Pc) return;
+            
+            var playerCharacter = (Character*)targetObject;
+            var homeWorldID = playerCharacter->HomeWorld;
+            
+            var homeWorldName = DataManager.GetExcelSheet<World>().GetRow(homeWorldID).Name.ExtractText();
 
+            var dataCenterName = DataManager.GetExcelSheet<World>().GetRow(homeWorldID).DataCenter.Value.Name.ExtractText();
+            
+            string regionName = string.Empty;
+
+            if(dataCenterName == "Aether" ||dataCenterName == "Crystal" ||dataCenterName == "Primal" ||dataCenterName == "Dynamis")
+            {
+                regionName = "NA";
+            }
+            else if (dataCenterName == "Chaos" || dataCenterName == "Light")
+            {
+                regionName = "EU";
+            }
+            else if (dataCenterName == "Elemental" || dataCenterName == "Gaia" || dataCenterName == "Mana" || dataCenterName == "Meteor")
+            {
+                regionName = "JP";
+            }
+            else if (dataCenterName == "Materia")
+            {
+                regionName = "OC";
+            }
+            
+            string url = "https://www.fflogs.com/character/"+ regionName +"/" + homeWorldName +  "/" + targetObject->NameString;
+        
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = url,
+                UseShellExecute = true
+            });
+        }
+    }
+    
     private void DrawUI() => WindowSystem.Draw();
-
-    public void ToggleConfigUI() => ConfigWindow.Toggle();
+    /*public void ToggleConfigUI() => ConfigWindow.Toggle();*/
     public void ToggleMainUI() => MainWindow.Toggle();
 }
